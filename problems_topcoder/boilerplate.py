f4a362c8a6d7cb98d2291e42c6d042804f96b031
Kirill Gugaev <gugaevkirill@gmail.com>
2018-03-09 14:34:28 +0300

    Add boilerplate

---
+++ problems_topcoder/boilerplate.py
@@ -0,0 +1,20 @@
+import time
+
+
+class ClassName:
+    def method_name(self):
+        start_time = time.time()
+
+        # Your code here
+        ans = 0
+
+        eval_time = round(time.time() - start_time, 4)
+        print(f'\n{eval_time} sec')
+
+        return ans
+
+obj = ClassName()
+
+# Methods tests:
+
+# Solution checks:
