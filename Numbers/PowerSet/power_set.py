7cdc8887ce4b78897b76f0e62a1fe667cdadce63
Kirill Gugaev <gugaevkirill@gmail.com>
2018-04-13 11:32:30 +0300

    PowerSet: update

--- Numbers/PowerSet/power_set.py
+++ Numbers/PowerSet/power_set.py
@@ -1,36 +1,44 @@
+#!/usr/bin/python3
 from typing import Set
-from collections import defaultdict, OrderedDict
+from collections import Counter
 
 
 def power_set_ranger(target: str) -> Set[str]:
-    letter_counts = defaultdict(int)
-    for l in target:
-        letter_counts[l] += 1
+    """
+    >>> power_set_ranger('')
+    ['']
     
-    letter_counts_sorted = OrderedDict(sorted(letter_counts.items()))
+    # Simple 1
+    >>> power_set_ranger('a')
+    ['', 'a']
     
-    power_set = {''}
-    for l, count_l in letter_counts_sorted.items():
-        new_elements = set()
-        for prefix in power_set:
-            for i in range(1, count_l + 1):
-                new_elements.add(prefix + l * i)
-        power_set = power_set.union(new_elements)
+    # Simple 2
+    >>> power_set_ranger('cba')
+    ['', 'a', 'b', 'ab', 'c', 'ac', 'bc', 'abc']
+    
+    # Duplicate letters
+    >>> power_set_ranger('aaa')
+    ['', 'a', 'aa', 'aaa']
+    
+    # Duplicate letters in random order
+    >>> power_set_ranger('aba')
+    ['', 'a', 'aa', 'b', 'ab', 'aab']
+    """
+
+    letter_counts = Counter(target)
+    letter_counts_sorted = sorted(letter_counts.items())
+    
+    power_set = ['']
+    for l, count_l in letter_counts_sorted:
+        power_set += [
+            prefix + l * i
+            for i in range(1, count_l + 1)
+            for prefix in power_set
+        ]
 
     return power_set
-        
 
-# Empty string
-assert {''} == power_set_ranger('')
 
-# Simple 1
-assert {'', 'a'} == power_set_ranger('a')
-
-# Simple 2
-assert {'', 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc'} == power_set_ranger('cba')
-
-# Duplicate letters
-assert {'', 'a', 'aa', 'aaa'} == power_set_ranger('aaa')
-
-# Duplicate letters in random order
-assert {'', 'a', 'b', 'aa', 'ab', 'aab'} == power_set_ranger('aba')
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
